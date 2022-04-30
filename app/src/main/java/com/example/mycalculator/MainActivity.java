069c3435257ea29b581ac5e91792eeb832687507
daniyal019 <daniyal019@users.noreply.github.com>
2022-05-01 01:39:24 +0500

    2nd commit

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.mycalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+     Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn0,btn10,btn11,btn12,btn13,btn14,btn15;
+     EditText text;
+     Float a,b,result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn1=(Button)findViewById(R.id.btn1);
+        btn2=(Button)findViewById(R.id.btn2);
+        btn3=(Button)findViewById(R.id.btn3);
+        btn4=(Button)findViewById(R.id.btn4);
+        btn5=(Button)findViewById(R.id.btn5);
+        btn6=(Button)findViewById(R.id.btn6);
+        btn7=(Button)findViewById(R.id.btn7);
+        btn8=(Button)findViewById(R.id.btn8);
+        btn9=(Button)findViewById(R.id.btn9);
+        btn10=(Button)findViewById(R.id.btn10);
+        btn11=(Button)findViewById(R.id.btn11);
+        btn12=(Button)findViewById(R.id.btn12);
+        btn13=(Button)findViewById(R.id.btn13);
+        btn14=(Button)findViewById(R.id.btn14);
+        btn15=(Button)findViewById(R.id.btn15);
+        text=(EditText)findViewById(R.id.txt);
     }
 }
